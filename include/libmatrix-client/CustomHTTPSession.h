1cf2b97ce3584ff848e79833ca74ca45867bc716
jaredoconnell <jared.oc321@gmail.com>
2020-06-21 10:10:33 -0400

    Added .gitignore and started with the HTTP session classes.

---
+++ include/libmatrix-client/CustomHTTPSession.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef CUSTOM_HTTP_SESSION
+#define CUSTOM_HTTP_SESSION
+
+
+
+#endif
---
+++ include/libmatrix-client/HTTPSession.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef HTTP_SESSION_H
+#define HTTP_SESSION_H
+
+#ifdef MATRIX_USE_CUSTOM_HTTP_SESSION
+#include "CustomHTTPSession.h"
+
+#endif
