bee5503f5856d10b050c9a9a6482a81af22a6f07
keith <keith@mailtux.net>
2020-06-25 03:27:47 +0000

    Adding Linter support (#3)
    Adding coala to lint our files using our Code Style Guidelines (that don't exist as of this current commit)

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char **argv){
-    std::cout << "Hello World, this is to test GitHub Action flows" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+	std::cout << "Hello World, this is to test GitHub Action flows" << std::endl;
+	return 0;
 }
