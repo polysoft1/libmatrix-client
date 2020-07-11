32ddc37bb4ff4a6ac119c87919d941806d5b2f51
Keith Valin <keith@mailtux.net>
2020-07-11 16:06:55 -0400

    Why does VSCode default to spaces for indentation

--- tests/HTTPTests.cpp
+++ tests/HTTPTests.cpp
@@ -8,48 +8,48 @@
 #include <chrono>
 
 BOOST_AUTO_TEST_CASE( example_org_GET_request ) {
-    LibMatrix::CPPRESTSDKSession session;
-    bool success = false;
-    bool running = true;
-    std::string error_msg;
+	LibMatrix::CPPRESTSDKSession session;
+	bool success = false;
+	bool running = true;
+	std::string error_msg;
 
-    session.setURL("http://example.org/");
-    session.setResponseCallback([&running, &success](LibMatrix::Response resp){
-        success = (resp.status == LibMatrix::HTTPStatus::HTTP_OK);
-        running = false;
-    });
-    session.request(LibMatrix::HTTPMethod::GET);
-    
-    while(running){
-        //Wait
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
+	session.setURL("http://example.org/");
+	session.setResponseCallback([&running, &success](LibMatrix::Response resp){
+		success = (resp.status == LibMatrix::HTTPStatus::HTTP_OK);
+		running = false;
+	});
+	session.request(LibMatrix::HTTPMethod::GET);
+	
+	while(running){
+		//Wait
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
 
-    BOOST_ASSERT(success);
+	BOOST_ASSERT(success);
 }
 
 BOOST_AUTO_TEST_CASE( invalid_TLD_test ) {
-    LibMatrix::CPPRESTSDKSession session;
-    bool success = false;
-    bool running = true;
-    std::string error_msg;
+	LibMatrix::CPPRESTSDKSession session;
+	bool success = false;
+	bool running = true;
+	std::string error_msg;
 
-    session.setURL("http://example.gfoakgoawogeriawejhgiogejiujwaegheujawgh/");
-    session.setResponseCallback([&running, &success](LibMatrix::Response resp){
-        success = false;
-        running = false;
-    });
-    session.setErrorCallback([&running, &success](std::string error) {
-        success = (error.compare("Error resolving address") == 0);
-        running = false;
-    });
+	session.setURL("http://example.gfoakgoawogeriawejhgiogejiujwaegheujawgh/");
+	session.setResponseCallback([&running, &success](LibMatrix::Response resp){
+		success = false;
+		running = false;
+	});
+	session.setErrorCallback([&running, &success](std::string error) {
+		success = (error.compare("Error resolving address") == 0);
+		running = false;
+	});
 
-    session.request(LibMatrix::HTTPMethod::GET);
-    
-    while(running){
-        //Wait
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
+	session.request(LibMatrix::HTTPMethod::GET);
+	
+	while(running){
+		//Wait
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
 
-    BOOST_ASSERT(success);
+	BOOST_ASSERT(success);
 }
