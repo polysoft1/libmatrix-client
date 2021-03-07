06e61ba6bbfd100f8a24b7d64fa8ba92388e2808
Keith Valin <keith@mailtux.net>
2021-03-07 18:00:43 -0500

    Fix the unit tests, and some linter fixes

--- src/MatrixSessionEncryption.cpp
+++ src/MatrixSessionEncryption.cpp
@@ -4,14 +4,14 @@
 #include "MatrixSession.h"
 #include "Exceptions.h"
 
-using namespace LibMatrix;
+using namespace LibMatrix; // Ignore CPPLintBear
 
-void MatrixSession::setupEncryptAccount(){
-    //Account generation
+void MatrixSession::setupEncryptAccount() {
+	//Account generation
 	uint8_t *buffer;
 	encryptAccountBuff = new uint8_t[olm_account_size()];
 	encryptAccount = olm_account(encryptAccountBuff);
-	
+
 	std::size_t random_size = olm_create_account_random_length(encryptAccount);
 	buffer = new uint8_t[random_size];
 	size_t retVal = olm_create_account(encryptAccount, buffer, random_size);
@@ -25,12 +25,12 @@ void MatrixSession::setupEncryptAccount(){
 		throw Exceptions::OLMException(err, Exceptions::ACC_CREATE);
 	}
 }
-void MatrixSession::genIdKeys(){
-    //ID Key generation
+void MatrixSession::genIdKeys() {
+	//ID Key generation
 	std::size_t idLength = olm_account_identity_keys_length(encryptAccount);
 	char *buffer = new char[idLength];
 
-	if(olm_account_identity_keys(encryptAccount, buffer, idLength) == olm_error()){
+	if(olm_account_identity_keys(encryptAccount, buffer, idLength) == olm_error()) {
 		std::string err = olm_account_last_error(encryptAccount);
 		delete[] buffer;
 		buffer = nullptr;
@@ -43,19 +43,19 @@ void MatrixSession::genIdKeys(){
 	buffer = nullptr;
 }
 void MatrixSession::clearEncryptAccount() {
-    olm_clear_account(encryptAccount);
+	olm_clear_account(encryptAccount);
 	delete[] encryptAccountBuff;
 
 	encryptAccountBuff = nullptr;
 	encryptAccount = nullptr;
-    
-    idKeys.clear();
+
+	idKeys.clear();
 }
 
-std::string MatrixSession::signMessage(std::string message){
-    std::size_t sigSize = olm_account_signature_length(encryptAccount);
+std::string MatrixSession::signMessage(std::string message) {
+	std::size_t sigSize = olm_account_signature_length(encryptAccount);
 	char *buffer = new char[sigSize];
-	if(olm_account_sign(encryptAccount, message.data(), message.size(), buffer, sigSize) == olm_error()){
+	if(olm_account_sign(encryptAccount, message.data(), message.size(), buffer, sigSize) == olm_error()) {
 		std::string err = olm_account_last_error(encryptAccount);
 		delete[] buffer;
 		buffer = nullptr;
@@ -68,5 +68,5 @@ std::string MatrixSession::signMessage(std::string message){
 	delete[] buffer;
 	buffer = nullptr;
 
-    return sig;
+	return sig;
 }
